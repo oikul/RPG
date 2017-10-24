dfe143b6aa3f909d071ca29a7643a2db20a9f72d
Zoostorm <Zoostorm@DESKTOP-MLB3RJS>
2017-10-24 19:04:09 +0100

    finished Block

--- RPG/src/main/Main.java
+++ RPG/src/main/Main.java
@@ -8,7 +8,7 @@ import utils.AbstractMain;
 public class Main extends AbstractMain {
 
 	private static final long serialVersionUID = 1L;
-	Graphics offG;
+	private Graphics offG;
 
 	@Override
 	public void initialise() {
--- RPG/src/world/Block.java
+++ RPG/src/world/Block.java
@@ -1,5 +1,6 @@
 package world;
 
+import java.awt.Graphics;
 import java.awt.Image;
 
 import handlers.ResourceHandler;
@@ -7,16 +8,35 @@ import handlers.ResourceHandler;
 public class Block {
 	
 	private String name;
-	private int frames, delay;
+	private int frames, currentFrame, width, height;
+	private long delay, currentTime, lastTime;
 	private boolean collideable;
 	private Image[] sprites;
 	
-	public Block(String name, String imagePath, int delay, boolean collideable){
+	public Block(String name, String imagePath, long delay, boolean collideable, int width, int height){
 		setName(name);
-		sprites = ResourceHandler.getBlockSprites(imagePath, 16, 16);
+		sprites = ResourceHandler.getBlockSprites(imagePath, width, height);
 		setFrames(sprites.length);
 		setDelay(delay);
 		setCollideable(collideable);
+		setWidth(width);
+		setHeight(height);
+	}
+	
+	public void update(){
+		currentTime = System.currentTimeMillis();
+		if(lastTime + delay <= currentTime){
+			if(currentFrame < sprites.length - 1){
+				currentFrame++;
+			}else{
+				currentFrame = 0;
+			}
+			lastTime = System.currentTimeMillis();
+		}
+	}
+	
+	public void draw(Graphics g, int x, int y, float scale){
+		g.drawImage(sprites[currentFrame], x, y, (int) (width * scale), (int) (height * scale), null);
 	}
 	
 	public String getName() {
@@ -35,11 +55,11 @@ public class Block {
 		this.frames = frames;
 	}
 	
-	public int getDelay() {
+	public long getDelay() {
 		return delay;
 	}
 	
-	public void setDelay(int delay) {
+	public void setDelay(long delay) {
 		this.delay = delay;
 	}
 	
@@ -49,6 +69,22 @@ public class Block {
 	
 	public void setCollideable(boolean collideable) {
 		this.collideable = collideable;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
 	}
 
 }
